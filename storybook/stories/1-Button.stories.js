0d8fa02e1e17835837f1ea0b14d610329adc6868
theWickedWebDev <syoung@paintnite.com>
2020-02-25 20:03:14 -0500

    :building_construction: Adding storybook

--- storybook/stories/1-Button.stories.js
+++ storybook/stories/1-Button.stories.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Button } from '@storybook/react/demo';
+import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
 
 export default {
   title: 'Button',
   component: Button,
+  decorators: [withKnobs]
 };
 
 export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
@@ -16,3 +18,19 @@ export const Emoji = () => (
     </span>
   </Button>
 );
+
+// Knobs for React props
+export const withAButton = () => (
+  <button disabled={boolean("Disabled", false)}>
+    {text("Label", "Hello Storybook")}
+  </button>
+);
+
+// Knobs as dynamic variables.
+export const asDynamicVariables = () => {
+  const name = text("Name", "James");
+  const age = number("Age", 35);
+  const content = `I am ${name} and I'm ${age} years old.`;
+
+  return <div>{content}</div>;
+};
